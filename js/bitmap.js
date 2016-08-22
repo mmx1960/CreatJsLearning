0533d4c68a7dddd57b8d6a5e3973cdfbd736577f
mmx1960 <3860953aa>
2016-08-23 00:13:52 +0800

    Bitmap初学

---
+++ js/bitmap.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Administrator on 2016/8/22 0022.
+ */
+var stage  = new createjs.Stage("gameView");
+var container = new createjs.Container();
+var change = true;
+stage.addChild(container);
+
+var img = new createjs.Bitmap("img/shabi.png");
+var img2 = new createjs.Bitmap("img/shabi2.png");
+container.addChild(img2);
+
+createjs.Ticker.setFPS(1);
+createjs.Ticker.addEventListener("tick",function(){
+    stage.update();
+    if(!change){
+        container.removeChild(img);
+        container.addChild(img2);
+        change = true;
+    }else
+    {
+        container.removeChild(img2);
+        container.addChild(img);
+        change = false;
+    }
+})
